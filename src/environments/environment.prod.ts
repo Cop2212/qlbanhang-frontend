a8b0485cd07c733e13a623ccd0e8f305c1c1336b
Cop2212 <129774517+Cop2212@users.noreply.github.com>
2026-04-11 18:28:51 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ten-mien-cua-ban.com/api'
+    apiUrl: 'https://qlbanhang-backend.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8000/api'
-};
-
 // export const environment = {
-//     production: true,
-//     apiUrl: 'https://qlbanhang-backend.onrender.com/api'
+//     production: false,
+//     apiUrl: 'http://localhost:8000/api'
 // };
+
+export const environment = {
+    production: true,
+    apiUrl: 'https://qlbanhang-backend.onrender.com/api'
+};
